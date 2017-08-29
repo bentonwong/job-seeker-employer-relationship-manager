b2c9de0997a7943b686cbb76eb7b084a8ea1c75a
bentonwong <bentonwong@gmail.com>
2017-08-28 22:22:00 -0700

    add new form and show page

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,8 +4,22 @@ class ApplicationsController < ApplicationController
     @applications = Application.all
   end
 
+  def new
+    @application = Application.new
+  end
+
+  def create
+    @application = Application.new(application_params)
+  end
+
   def show
     @application = Application.find_by(id: params[:id])
   end
+
+  private
+
+    def application_params
+      params.require(:application).permit(:company, :position, :description, :notes)
+    end
 
 end
