a7cf3beb8cfe8c9d4a8dee0e039b236d5975fc87
bentonwong <bentonwong@gmail.com>
2017-08-28 22:11:33 -0700

    add new and show methods in controller %>

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,4 +2,12 @@ class Application < ApplicationRecord
   def index
     @applications = Application.all
   end
+
+  def new
+    @application = Application.new
+  end
+  
+  def show
+    @application = Application.find_by(id: params[:id])
+  end
 end
