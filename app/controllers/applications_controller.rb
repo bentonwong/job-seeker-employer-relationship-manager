57a85380b456ea13dac5d991832481720f31b8e6
bentonwong <bentonwong@gmail.com>
2017-08-28 22:51:40 -0700

    add edit page and controllers

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -21,6 +21,19 @@ class ApplicationsController < ApplicationController
     @application = Application.find_by(id: params[:id])
   end
 
+  def edit
+    @application = Application.find_by(id: params[:id])
+  end
+
+  def update
+    @application = Application.find_by(id: params[:id])
+    if @application.update(application_params)
+      redirect_to @application
+    else
+      redirect_to edit_application_path(@application)
+    end
+  end
+
   private
 
     def application_params
