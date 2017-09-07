235c2f7bb729d2dad9494dd2fc0279a78dbd37b8
bentonwong <bentonwong@gmail.com>
2017-09-07 15:26:50 -0700

    start coding regex in email model for matching to an application

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,6 +1,13 @@
 class Email < ApplicationRecord
   def self.receive_email(message)
+    #regex (?<=@)[^.]+ to parse the domain
+    #use .match
     email_id = message.subject[/^Update (\d+)$/, 1]
+    extracted_email_domains = []
+    extracted_email_domains.push(message.from[(?<=@)[^.]+, 1])
+    extracted_email_domains.push(message.to[(?<=@)[^.]+, 1])
+    extracted_email_domains.push(message.body[(?<=@)[^.]+, 1])
+
     if email_id.present?
       Email.update(email_id, body: message.body.decoded)
     else
