d96983a6b4c00fd5dbb783428c671d8395fd2cfc
bentonwong <bentonwong@gmail.com>
2017-09-19 18:55:09 -0400

    fix display of associated emails

--- app/models/email.rb
+++ app/models/email.rb
@@ -7,7 +7,7 @@ class Email < ApplicationRecord
     #extracted_emails =[]
     #extracted_emails += message.to.first[/^(?=@).+($)/, 1]
     #extracted_emails += message.text_part.body.decoded[/^(?=@).+($)/, 1]
-    Email.create! subject: message.subject, body: message.text_part.body.decoded, from: message.from, to: message.to, date: message.date
+    Email.create! subject: message.subject, body: message.multipart? ? message.text_part.body.decoded : message.body.decoded, from: message.from, to: message.to, date: message.date
 
     #if email_id.present?
       #Email.update(email_id, body: message.body.decoded)
