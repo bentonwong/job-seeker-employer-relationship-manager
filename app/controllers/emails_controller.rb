92536e60034af26e6b235f1d451dace1a60090a4
bentonwong <bentonwong@gmail.com>
2017-09-19 15:22:24 -0400

    update email controller and views

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,2 +1,29 @@
 class EmailsController < ApplicationController
+  def index
+    @emails = Email.all
+  end
+
+  def show
+    @email = Email.find_by(id: params[:id])
+  end
+
+  def edit
+    @email = Email.find_by(id: params[:id])
+  end
+
+  def update
+    @email = Email.find_by(id: params[:id])
+    if @email.update(email_params)
+      redirect_to @email
+    else
+      redirect_to edit_application_path(@email)
+    end
+  end
+
+  private
+
+    def email_params
+      params.require(:email).permit(:application_id)
+    end
+
 end
--- app/models/email.rb
+++ app/models/email.rb
@@ -1,17 +1,18 @@
+require 'pry'
+
 class Email < ApplicationRecord
   def self.receive_email(message)
-    #regex (?<=@)[^.]+ to parse the domain
     #use .match
-    email_id = message.subject[/^Update (\d+)$/, 1]
-    extracted_email_domains = []
-    extracted_email_domains.push(message.from[(?<=@)[^.]+, 1])
-    extracted_email_domains.push(message.to[(?<=@)[^.]+, 1])
-    extracted_email_domains.push(message.body[(?<=@)[^.]+, 1])
+    #regex: (?<=@).+($)
+    #extracted_emails =[]
+    #extracted_emails += message.to.first[/^(?=@).+($)/, 1]
+    #extracted_emails += message.text_part.body.decoded[/^(?=@).+($)/, 1]
+    Email.create! subject: message.subject, body: message.text_part.body.decoded, from: message.from, to: message.to, date: message.date
 
-    if email_id.present?
-      Email.update(email_id, body: message.body.decoded)
-    else
-      Email.create! subject: message.subject, body: message.text_part.body.decoded, from: message.from.first, to: message.from.first, date: message.date
-    end
+    #if email_id.present?
+      #Email.update(email_id, body: message.body.decoded)
+    #else
+      #Email.create! subject: message.subject, body: message.text_part.body.decoded, from: message.from.first, to: message.to.first, date: message.date
+    #end
   end
 end
