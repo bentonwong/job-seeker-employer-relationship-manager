f17a32f302953509f7459947735d759e6395fde6
bentonwong <bentonwong@gmail.com>
2017-10-04 15:08:31 -0700

    start coding company controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,17 @@
 class CompaniesController < ApplicationController
+
+  def index
+    @companies = Company.all
+  end
+
+  def create
+    #company_name = params[company_name]
+    #results = fetch data from api company name
+    #http://api.glassdoor.com/api/api.htm?t.p={GLASSDOOR_PARTNER_ID}&t.k=GLASSDOOR_KEY&userip=0.0.0.0&useragent=&format=json&v=1&action=employers
+    #@company = results
+  end
+
+  def update
+  end
+
 end
