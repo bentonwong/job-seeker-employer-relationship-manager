c36286a27b660599b7fef4014e6b12461906c820
bentonwong <bentonwong@gmail.com>
2017-10-11 11:15:27 -0700

    integrate glassdoor company search into application edit page

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,7 +3,6 @@ class CompaniesController < ApplicationController
   def search
   end
 
-
   def glassdoor_search
     begin
       @resp = Faraday.get "http://api.glassdoor.com/api/api.htm?" do |req|
@@ -11,21 +10,21 @@ class CompaniesController < ApplicationController
         req.params['format'] = "json"
         req.params['t.p'] = ENV["GLASSDOOR_PARTNER_ID"]
         req.params['t.k'] = ENV["GLASSDOOR_KEY"]
-        req.params['userip'] = '192.168.43.42'
-        req.params['useragent'] = "Mozilla/5.0"
+        req.params['userip'] = '192.168.43.42' #find command to extract from client
+        req.params['useragent'] = "Mozilla/5.0" #find command to extract from client
         req.params['q'] = params[:query]
         req.params['action'] = "employers"
       end
       body = JSON.parse(@resp.body)
       if @resp.success?
-        @company = body
+        @company = body["response"]["employers"]
       else
         @error = body["meta"["errorDetail"]]
       end
       rescue Faraday::TimeoutError
         @error = "There was a timeout. Please try again."
-       end
-       render json: @company
+      end
+      render json: @company
     end
 
 end
